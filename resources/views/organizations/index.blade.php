64503da456f4b6ee48d0fd1a3e65cd8a87a4c131
muhadif <muhadifff@gmail.com>
2021-07-02 20:42:58 +0700

    remove comment on pagination view

--- resources/views/organizations/index.blade.php
+++ resources/views/organizations/index.blade.php
@@ -71,11 +71,7 @@
     </table>
 
 
-    <input type="hidden" name="hidden_page" id="hidden_page" value="1" />
-    <input type="hidden" name="hidden_column_name" id="hidden_column_name" value="id" />
-    <input type="hidden" name="hidden_sort_type" id="hidden_sort_type" value="asc" />
-
-{{--    {!! $data->links() !!}--}}
+    {!! $data->render() !!}
 
     <p class="text-center text-primary"><small>muhadif here <3</small></p>
 
